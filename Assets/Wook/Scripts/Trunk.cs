01380c58f7a061f83d020f09dafeb47252f302ad
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-03-20 03:30:28 +0900

    그리드 채우기

---
+++ Assets/Wook/Scripts/Trunk.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trunk : MonoBehaviour
+{
+    float Scale_x;
+    float Scale_y;
+    float Pos_y;
+    float Growth_Speed;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
