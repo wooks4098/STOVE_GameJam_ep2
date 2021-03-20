516e1a046b9b25a46c6b985b58cbde5e2e587cbc
Moty <not9501@naver.com>
2021-03-20 19:17:47 +0900

    UI Cost

--- Assets/THScripts/Scripts/UIColorCost.cs
+++ Assets/THScripts/Scripts/UIColorCost.cs
@@ -8,11 +8,16 @@ public class UIColorCost : MonoBehaviour
     [SerializeField]
     private Chalet chalet;
     [SerializeField]
+    private Text colorCountText;
+    [SerializeField]
+    private Text pixelCountText;
+    [SerializeField]
     private Text costText;
 
-    
     void Update()
     {
-        costText.text = "Cost : " + chalet.SelectedColorCount.ToString();
+        costText.text = "Cost : " + chalet.Cost.ToString();
+        colorCountText.text = "Color : " + chalet.SelectedColorCount.ToString();
+        pixelCountText.text = "Pixel : " + chalet.PixelCount.ToString();
     }
 }
