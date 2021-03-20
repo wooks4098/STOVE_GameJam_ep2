566e8aeea462e553e525fbd69a2bc1d4ffffbe74
com8599 <com8599@gmail.com>
2021-03-21 08:54:13 +0900

    main에는 머지하기전에 항상 말해주세요

--- Assets/Wook/Scripts/Grow/Trunk_.cs
+++ Assets/Wook/Scripts/Grow/Trunk_.cs
@@ -38,6 +38,9 @@ public class Trunk_ : MonoBehaviour
     public GameObject Flower;
     GameObject Camera;
 
+    public static float start_time;
+
+
     private void Awake()
     {
         Camera = GameObject.FindWithTag("MainCamera");
@@ -66,6 +69,7 @@ public class Trunk_ : MonoBehaviour
                 if(IsFlower)
                 {
                     Flower.SetActive(true);
+                    start_time = Time.deltaTime;        //꽃이 켜지면 시작시간을 측정한다.
                 }
                 else
                 {
